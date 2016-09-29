93c0c410c35353e2291d62cc956e10827ac5a95a
rohan-ratnaparkhi <rohanr@talenticaadmins-Mac-mini-3.local>
2016-09-29 16:52:31 +0530

    adding navigation drawer to home screen

--- BookShelfApp/HomeViewController.swift
+++ BookShelfApp/HomeViewController.swift
@@ -12,6 +12,33 @@ class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        attachLeftNavigationDrawer()
+        
+    }
+    
+    func attachLeftNavigationDrawer() {
+        var centerContainer: MMDrawerController?
+        
+        let appDelegate: AppDelegate = UIApplication.shared.delegate as! AppDelegate
+        let appWindow = appDelegate.window
+        
+        let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        let centerViewController = self.navigationController
+        let leftViewController = mainStoryboard.instantiateViewController(withIdentifier: "LeftSideNavigationViewController") as! LeftSideNavigationViewController
+
+        let leftSideNav = UINavigationController(rootViewController: leftViewController)
+        //let centerNav = UINavigationController(rootViewController: centerViewController)
+        let centerNav = centerViewController
+        
+        centerContainer = MMDrawerController(center: centerNav, leftDrawerViewController: leftSideNav)
+        
+        centerContainer!.openDrawerGestureModeMask = MMOpenDrawerGestureMode.panningCenterView;
+        centerContainer!.closeDrawerGestureModeMask = MMCloseDrawerGestureMode.panningCenterView;
+        
+        appWindow!.rootViewController = centerContainer
+        appWindow!.makeKeyAndVisible()
+        
     }
     
 }
---
+++ BookShelfApp/LeftSideNavigationViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LeftSideNavigationViewController.swift
+//  BookShelfApp
+//
+//  Created by Rohan Ratnaparkhi on 9/29/16.
+//  Copyright © 2016 Talentica. All rights reserved.
+//
+
+import UIKit
+
+class LeftSideNavigationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
