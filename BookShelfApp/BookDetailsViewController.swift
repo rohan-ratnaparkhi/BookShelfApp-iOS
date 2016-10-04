2eb800e49875814d99bfe9d32e1bd85a1ffa97fd
rohan-ratnaparkhi <rohanr@talenticaadmins-Mac-mini-3.local>
2016-10-04 12:51:08 +0530

    UI for Book Details screen

---
+++ BookShelfApp/BookDetailsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BookDetailsViewController.swift
+//  BookShelfApp
+//
+//  Created by Rohan Ratnaparkhi on 10/3/16.
+//  Copyright © 2016 Talentica. All rights reserved.
+//
+
+import UIKit
+
+class BookDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
